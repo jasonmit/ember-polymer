a5699fd8d862ed92d0449f3cc164db5556213a3c
Sam Clopton <samsinite@gmail.com>
2014-12-29 23:08:58 -0800

    added npm dependences to install blueprint

--- blueprints/ember-polymer/index.js
+++ blueprints/ember-polymer/index.js
@@ -2,14 +2,20 @@
 
 module.exports = {
   afterInstall: function() {
+    var self = this;
     // this.addPackageToProject("broccoli-static-compiler", "^0.2.1");
     // this.addPackageToProject("broccoli-vulcanize", "^1.0.4");
+    return this.addPackagesToProject([
+      { name: "broccoli-static-compiler", target: "^0.2.1" },
+      { name: "broccoli-vulcanize", target: "^1.0.4" }
+    ]).then(function() {
+      return self.addBowerPackagesToProject([
+        { name: "Polymer/polymer", target: "~0.5.2" },
+        { name: "Polymer/core-elements", target: "~0.5.2" },
+        { name: "Polymer/paper-elements", target: "~0.5.2" }
+      ]);
+    });
 
-    return this.addBowerPackagesToProject([
-      { name: "Polymer/polymer", target: "~0.5.2" },
-      { name: "Polymer/core-elements", target: "~0.5.2" },
-      { name: "Polymer/paper-elements", target: "~0.5.2" }
-    ]);
   },
 
   normalizeEntityName: function() {}
